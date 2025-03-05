12627dcad5904828fad106d86882f17f6cd748c1
nguyenphuochuyy <phuochuy10023@gmail.com>
2025-03-05 17:57:34 +0700

    update file docker & docker-compose

--- Auth-Service/src/main/java/fit/iuh/AuthenApplication.java
+++ Auth-Service/src/main/java/fit/iuh/AuthenApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserServiceApplication {
+public class AuthenApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
+        SpringApplication.run(AuthenApplication.class, args);
     }
 
 }
