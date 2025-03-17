fbfe7af8d264c21d71630dd2f02ec5d7f89836b8
TRANQUOCANH\quoca <quocanh1609.iuh@gmail.com>
2025-03-17 22:02:46 +0700

    complete api get all product

---
+++ Product-Service/src/main/java/iuh/fit/se/controllers/ProductController.java
@@ -0,0 +1,22 @@
+package iuh.fit.se.controllers;
+
+import iuh.fit.se.models.Product;
+import iuh.fit.se.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
